4f0355ef4852581134a89a07d05d2d7d15c9f00b
Adrián Canitano <acanitano@gmail.com>
2021-06-21 18:16:44 -0300

    Creacion de proyecto

---
+++ src/main/java/ar/com/springboot/ms/config/SpringbootConfigServerApplication.java
@@ -0,0 +1,13 @@
+package ar.com.springboot.ms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/springboot/ms/config/SpringbootConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.springboot.ms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
