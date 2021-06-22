b9fda9d012f375d1635b12e3518efbaa96fee572
Adrián Canitano <acanitano@gmail.com>
2021-06-22 17:17:33 -0300

    config

--- src/main/java/ar/com/springboot/ms/config/SpringbootConfigServerApplication.java
+++ src/main/java/ar/com/springboot/ms/config/SpringbootConfigServerApplication.java
@@ -2,7 +2,9 @@ package ar.com.springboot.ms.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringbootConfigServerApplication {
 
